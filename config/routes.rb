10b4277dc96cf1d7184f99b0c47cd73807e08b37
Danil Pismenny <danil@orionet.ru>
2009-08-07 20:31:28 +0400

    follows and twits models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :twits
+
+  
+  map.signup 'signup', :controller => 'users', :action => 'new'
+  map.logout 'logout', :controller => 'sessions', :action => 'destroy'
+  map.login 'login', :controller => 'sessions', :action => 'new'
+  map.resources :sessions
+
+  map.resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
@@ -38,6 +48,8 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
+  
+  map.root :controller => 'home'
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
