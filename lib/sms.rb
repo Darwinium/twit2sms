76178c0933234493ec44d08b9fc0a030946486f6
Danil Pismenny <danil@orionet.ru>
2009-08-10 21:21:37 +0400

    some fixes

--- lib/sms.rb
+++ lib/sms.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 class SMS
   def self.send_sms(phone,message)
-    raise "Посылаю только на 79023270019" unless phone.to_s=="79023270019"
+#    raise "Посылаю только на 79023270019" unless phone.to_s=="79023270019"
     #       my $converter = Text::Iconv->new("koi8-r", "cp1251");
     # my $message=uri_escape($converter->convert($message));
     message=URI.escape(Iconv.iconv('cp1251','utf-8',message).to_s)
